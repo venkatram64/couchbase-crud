c21641faaed44981fc26bf3f9ae109ea5832314b
venkatram64 <venkat.veerareddy@hotmail.com>
2025-05-04 13:04:28 +0530

    init add

--- couch-base-crud/src/main/java/com/venkat/couch_base_crud/config/CouchbaseConfig.java
+++ couch-base-crud/src/main/java/com/venkat/couch_base_crud/config/CouchbaseConfig.java
@@ -1,12 +1,13 @@
 package com.venkat.couch_base_crud.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 
 @Configuration
 public class CouchbaseConfig extends AbstractCouchbaseConfiguration {
 
-    @Override
+    /*@Override
     public String getConnectionString() {
         return "couchbase://localhost";
     }
@@ -24,6 +25,38 @@ public class CouchbaseConfig extends AbstractCouchbaseConfiguration {
     @Override
     public String getBucketName() {
         return "employee";
+    }*/
+
+    @Value("${spring.couchbase.connection-string}")
+    private String connectionString;
+
+    @Value("${spring.couchbase.username}")
+    private String userName;
+
+    @Value("${spring.couchbase.password}")
+    private String password;
+
+    @Value("${spring.couchbase.bucket.name}")
+    private String bucketName;
+
+    @Override
+    public String getConnectionString() {
+        return connectionString;
+    }
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getBucketName() {
+        return bucketName;
     }
 
 
